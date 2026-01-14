af1e136ef0b6703572f7ac0fe8f7594618ab5c81
djartes20-lgtm <djartes20@gmail.com>
2026-01-14 10:47:42 -0400

    Update Fila.tsx

--- app/students/components/Fila.tsx
+++ app/students/components/Fila.tsx
@@ -6,7 +6,7 @@ interface FilaProps {
 
 export default function Fila({ fila }: FilaProps) {
   return (
-    <div className="card">
+    <div className="card p-3">
       <h2>Fila de Espera</h2>
       <ul>
         {fila.map((m) => (
