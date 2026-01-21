96a05a7782ca8fdea35ee605434fbff499d9b2aa
joaovitornara10 <54191082+joaovitornara10@users.noreply.github.com>
2026-01-21 09:17:03 -0400

    Update page.tsx

--- app/adm-dj/page.tsx
+++ app/adm-dj/page.tsx
@@ -2,11 +2,11 @@
 
 import Player from "@/app/components/Player";
 import Queue from "@/app/components/Queue";
-/*
+
 import History from "@/app/components/History";
 import ProfileMenu from "@/app/components/ProfileMenu";
 import ModeSelector from "@/app/components/ModeSelector";
-*/
+
 export default function AdminPage() {
   return (
     <div className="min-h-screen p-6 space-y-6">
@@ -17,10 +17,10 @@ export default function AdminPage() {
 
       <Player />
       <Queue />
-      {/*
+      
       <History />
       <ModeSelector />      <ProfileMenu />
-*/}
+
     </div>
   );
 }
