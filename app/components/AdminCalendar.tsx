6e70485704b5f8b9f49730ab78ed9e92f7c21c2c
djartes20-lgtm <djartes20@gmail.com>
2026-02-11 12:26:54 -0400

    11

--- app/components/AdminCalendar.tsx
+++ app/components/AdminCalendar.tsx
@@ -177,7 +177,7 @@ export default function AdminCalendar({ isAdmin }: Props) {
         .container {
           color: #ff0707;
           padding: 20px;
-          border: 4px solid #ff0707; /* Quadrado vermelho em volta de tudo */
+          border: 2px solid #ff0707; /* Quadrado vermelho em volta de tudo */
           border-radius: 12px;
         }
 
